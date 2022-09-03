837bd4b1c4c590881abbd250f84f878fcf2c17e5
Jonathan Netto <62727843+JonathanNetto@users.noreply.github.com>
2022-09-03 00:34:57 -0300

    Add files via upload

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyBVrAn5TG1VoNCyD1LRrOrNlU1SOEmmPOc",
+
+authDomain: "mundo-invertido-4827e.firebaseapp.com",
+
+projectId: "mundo-invertido-4827e",
+
+storageBucket: "mundo-invertido-4827e.appspot.com",
+
+messagingSenderId: "747923790027",
+
+appId: "1:747923790027:web:e70e84e5577f78afc6234e"
+
+};
+
+
+ // Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+  
+export default app
